523926827baa0a00544d267e0c794c604b606956
stanzealot <stanzealot@gmail.com>
2023-07-01 16:58:00 +0100

    nav andriod

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,11 +4,46 @@ import SideNav from '../../components/Sidebar/SideNav'
 import './Home.css'
 import Aside from '../../components/Sidebar/Aside'
 import MobileFooter from '../../components/footer/MobileFooter'
-
+import SideNavData from '../../components/Sidebar/SideNavData.json'
 function JobDetails() {
+  const allSideNavData = SideNavData;
   return (
     <div className='job-detail '>
         <Nav />
+       <div className='sidebar-menu-main'>
+          
+          <div className='andriod-profile'>
+            <img className='andriod-img' src={process.env.PUBLIC_URL+"/images/profileIcon.svg"} alt="ima" />
+            <h1 className='andrioid-name'>Dr. Arvind K</h1>
+            <p className='andrioid-update'>Update profile</p>
+          </div>
+          <div className='andrioid-line'></div>
+
+          <div className='sidenav-menu-container'>
+          {
+            allSideNavData.map((item)=>(
+
+              <div className={`sidenav-menu ${item.subNav ? "sub-down-space":"down-space"}`} key={item.id}>
+                <div className='sidenav-item'>
+                  <img src={process.env.PUBLIC_URL+item.icon} alt="item.title"/>
+                  <h1 className='sidenav-item-title'>{item.title}</h1>
+                  
+                </div>
+                
+                {
+                  item.subNav && item.subNav.map((subItem,index)=>(
+                    <div className='sidenav-subItem' key={index}>
+                        <p className='sidenav-subItem-text'>{subItem.subTitle}</p>
+                    </div>
+                  ))
+
+                }
+
+              </div>
+            ))
+          }
+       </div>
+       </div>
         <div className='job-detail-main'>
             <div className='job-detail-main-wrapper d-flex flex-row justify-content-between'>
                 <SideNav />
