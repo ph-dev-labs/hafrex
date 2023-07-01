e95baa8ac543768ee92945573e64100840e273da
stanzealot <stanzealot@gmail.com>
2023-07-01 12:25:30 +0100

    added jobDetail page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
+import { Routes,Route } from 'react-router-dom';
 import Main from "./Pages/Main/Main";
 import AllJobs from "./Pages/ALL-Jobs/AllJobs";
+import JobDetails from "./Pages/Job-Details/JobDetails";
 function App() {
   return (
+    
+      // <Routes>
+      //   <Route path='/' element={<JobDetails />} />
+      //   <Route path='/alljobs' element={<AllJobs />} />   
+      // </Routes>
+    
     <div className="App">
       <div className="d-flex flex-column">
         <Navbar />
@@ -14,11 +22,15 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Main />} />
             <Route path="/alljobs" element={<AllJobs />} />
+            <Route path="/alljobs" element={<AllJobs />} />
           </Routes>
           <Sidebar empty />
         </div>
       </div>
+
     </div>
+
+  
   );
 }
 
---
+++ src/components/Navbar/Nav.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Navbar.css'
+import {FaBars} from 'react-icons/fa'
+function Nav() {
+  return (
+    <div className='nav'>
+      <div className='nav-wrapper'>
+        <div className='nav-left'>
+            <img className='nav-logo' src={process.env.PUBLIC_URL+"/images/LogoIcon.svg"} alt="logo"/>
+            <h1 className='logo-title'>Securing Digital World!</h1>
+        </div>
+        <div className='nav-right'>
+            <div className='nav-all-jobs nav-item'>
+                <img className='nav-icon' src={process.env.PUBLIC_URL+"/images/JobIcon.svg"} alt="Alljob" />
+                <p>All Jobs</p>
+            </div>
+            <div className='nav-candidate nav-item'>
+                <img className='nav-icon' src={process.env.PUBLIC_URL+"/images/candidateIcon.svg"} alt="candidate" />
+                <p>Add a Candidate</p>
+            </div>
+            <div className='nav-profile nav-item'>
+                <img className='nav-icon' src={process.env.PUBLIC_URL+"/images/profileIcon.svg"} alt="profile" />
+                <div className='nav-profile-arrow'>
+                    <p>My Profile</p>
+                    <img src={process.env.PUBLIC_URL+"/images/profileArrow.svg"} alt='arrow' />
+                </div>
+            </div>
+        </div>
+      </div>
+        <div className='mobile-nav'>
+          <img src={process.env.PUBLIC_URL+"/images/mobileNavIcon.svg"} alt="mobileIcon"/>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
