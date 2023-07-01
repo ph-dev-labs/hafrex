d67c35d46ecbe44e1dca970bea856d1a626831f2
ph-dev-labs <85322356+ph-dev-labs@users.noreply.github.com>
2023-07-01 12:41:38 +0100

    mm

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import Main from "./Pages/Main/Main";
 import AllJobs from "./Pages/ALL-Jobs/AllJobs";
+import Refer from "./Pages/Refer/Refer";
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Main />} />
             <Route path="/alljobs" element={<AllJobs />} />
+            <Route path="/refercandidate" element={<Refer />} />
           </Routes>
           <Sidebar empty />
         </div>
